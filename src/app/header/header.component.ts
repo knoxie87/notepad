d3d3d24cae4233fe1726fe0e2e7fcce765af42c1
knoxie87 <andrew.knox87@gmail.com>
2024-08-26 20:34:13 +1200

    chore: Update app component and header component
    The app component and header component have been updated to include new HTML elements and styles. The app component now wraps the header, router outlet, and footer in a flex container. The header component now includes a navigation bar with buttons for saving, deleting, and creating notes.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatButton} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule], // Add MatIconModule to imports
+  imports: [MatTooltip, MatButtonModule, MatIconModule, MatDividerModule, MatButton], // Add MatIconModule to imports
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-purple': '#673ab7',
+      },
+    },
   },
   plugins: [],
 }
